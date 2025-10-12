accb5353114d9131085f8e6154a164302fe2a833
Danish Mirza Beg <dmb34208@gmail.com>
2025-10-12 12:36:32 +0530

    floor sqrt of num

--- binary search/BS_L10.cpp
+++ binary search/BS_L10.cpp
@@ -1,35 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
-    int singleNonDuplicate(vector<int>& arr) {
-        int n = arr.size();
-        if(n==1) return arr[0];
-        if(arr[0] != arr[1]) return arr[0];
-        if(arr[n-1] != arr[n-2]) return arr[n-1];
-        int low = 1, high = n - 2;
+    int sqrtnum(int n) {
+        int ans=n;
+        vector<int> arr;
+        for(int i=0;i<n;i++){
+            arr.push_back(i);
+        }
+        int low = 1, high = n - 1;
         while(low <= high){
             int mid = (low + high) / 2;
-            if(arr[mid] != arr[mid+1] && arr[mid] != arr[mid-1]) {
-                return arr[mid];
-            }
-            if((mid % 2 == 1 && arr[mid] == arr[mid-1]) || (mid % 2 == 0 && arr[mid] == arr[mid+1])) {
-                low = mid + 1;
+            if(mid*mid<=n) {
+                ans=mid;
+                low=mid+1;
             }
             else {
-                high = mid - 1;
+                high=mid-1;
             }
         }
-        return -1;
+        return ans;
 }
 int main(){
     int n;
-    cout<<"enter size of array"<<endl;
+    cout<<"enter the number"<<endl;
     cin>>n;
-    vector<int> nums;
-    for(int i=0;i<n;i++){
-        int x;
-        cout<<"enter element "<<i<<" "<<endl;
-        cin>>x;
-        nums.push_back(x);
-    }
-    cout<<singleNonDuplicate(nums);
+    cout<<sqrtnum(n);
 }
