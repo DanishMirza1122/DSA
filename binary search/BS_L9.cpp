4aeb9a25795b66be372e43bed5dcad552da024df
Danish Mirza Beg <dmb34208@gmail.com>
2025-10-12 12:03:03 +0530

    peak element

--- binary search/BS_L9.cpp
+++ binary search/BS_L9.cpp
@@ -1,22 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
-    int singleNonDuplicate(vector<int>& arr) {
+    int peak(vector<int>& arr) {
         int n = arr.size();
-        if(n==1) return arr[0];
-        if(arr[0] != arr[1]) return arr[0];
-        if(arr[n-1] != arr[n-2]) return arr[n-1];
-        int low = 1, high = n - 2;
-        while(low <= high){
-            int mid = (low + high) / 2;
-            if(arr[mid] != arr[mid+1] && arr[mid] != arr[mid-1]) {
-                return arr[mid];
-            }
-            if((mid % 2 == 1 && arr[mid] == arr[mid-1]) || (mid % 2 == 0 && arr[mid] == arr[mid+1])) {
-                low = mid + 1;
-            }
-            else {
-                high = mid - 1;
-            }
+        if(n==1) return 0;
+        if(arr[0]> arr[1]) return 0;
+        if(arr[n-1]>arr[n-2]) return n-1;
+        int low=1, high=n-2;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(arr[mid]>arr[mid+1]&& arr[mid]> arr[mid-1]) return mid;
+            else if(arr[mid]> arr[mid-1]) low=mid+1;
+            else high=mid-1;
         }
         return -1;
 }
@@ -31,5 +25,5 @@ int main(){
         cin>>x;
         nums.push_back(x);
     }
-    cout<<singleNonDuplicate(nums);
+    cout<<peak(nums);
 }
