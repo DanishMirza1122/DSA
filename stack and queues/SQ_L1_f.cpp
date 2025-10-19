d7338ae22fb8719d8b7bc2e103ba692a73432fa6
Danish Mirza Beg <dmb34208@gmail.com>
2025-10-19 12:35:30 +0530

    queue using stack

---
+++ stack and queues/SQ_L1_f.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Queue {
+public:
+    stack<int> s1,s2;
+    
+    void push(int x) {
+        while(s1.size()){
+            s2.push(s1.top());
+            s1.pop();
+        }
+        s1.push(x);
+        while(s2.size()){
+            s1.push(s2.top());
+            s2.pop();
+        }
+    }
+    
+    int pop() {
+        int n=s1.top();
+        s1.pop();
+        return n;
+    }
+    
+    int peek() {
+        return s1.top();
+    }
+    
+    bool empty() {
+        return s1.size()==0;
+    }
+    int size(){
+        return s1.size();
+    }
+};
+int main() {
+  Queue q;
+  q.push(3);
+  q.push(4);
+  cout << "The element poped is " << q.pop() << endl;
+  q.push(5);
+  cout << "The top of the queue is " << q.peek() << endl;
+  cout << "The size of the queue is " << q.size() << endl;
+}
