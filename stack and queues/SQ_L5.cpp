392f4653ee5e152fd476d4da2f76110df4fad5ce
Danish Mirza Beg <dmb34208@gmail.com>
2025-12-12 15:38:15 +0530

    nge 1

---
+++ stack and queues/SQ_L5.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> NGE(const vector<int>& arr){
+    int n = (int)arr.size();
+    vector<int> nge(n);
+    stack<int> st;
+
+    for(int i = n - 1; i >= 0; --i){
+        while(!st.empty() && st.top() <= arr[i])
+            st.pop();
+
+        if(st.empty())
+            nge[i] = -1;
+        else
+            nge[i] = st.top();
+
+        st.push(arr[i]);
+    }
+
+    return nge;
+}
+
+int main(){
+    int n;
+    if(!(cin >> n)) return 0;
+    vector<int> arr(n);
+    for(int i = 0; i < n; ++i) cin >> arr[i];
+
+    vector<int> result = NGE(arr);
+
+    for(int x : result) cout << x << ' ';
+    cout << '\n';
+    return 0;
+}
