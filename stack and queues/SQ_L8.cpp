309b64b314319d20b2614dc15cf9762291be3151
Danish Mirza Beg <dmb34208@gmail.com>
2025-12-21 00:14:41 +0530

    trap rainwater

---
+++ stack and queues/SQ_L8.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+    int trap(vector<int>& arr) {
+        int total=0;
+        int lmax=0;
+        int rmax=0;
+        int r=arr.size()-1;
+        int l=0;
+        while(l<r){
+            if(arr[l]<=arr[r]){
+                if(lmax>arr[l]){
+                    total+=lmax-arr[l];
+                }
+                else lmax=arr[l];
+                l=l+1;
+            }
+            else{
+                if(rmax>arr[r]){
+                    total+=rmax-arr[r];
+                }
+                else rmax=arr[r];
+                r=r-1;
+            }
+        }
+        return total;
+    }
