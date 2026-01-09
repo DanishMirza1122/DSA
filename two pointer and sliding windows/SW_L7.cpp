bf323c316b02ccd0a97885991faedc4f17616185
Danish Mirza Beg <dmb34208@gmail.com>
2026-01-09 19:10:51 +0530

    no of substring containing all three char

---
+++ two pointer and sliding windows/SW_L7.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int numberOfSubstrings(string s) {
+        int lastseen[3]={-1,-1,-1};
+        int n=s.size();
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            lastseen[s[i]-'a']=i;
+            cnt=cnt+1+min(lastseen[0],min(lastseen[1],lastseen[2]));
+        }
+        return cnt;
+}
