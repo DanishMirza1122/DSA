dd4ce987e4e8ed79cc125a9c4ec7650ae6a08452
Danish Mirza Beg <dmb34208@gmail.com>
2026-01-14 02:30:40 +0530

    nice subarrays

---
+++ two pointer and sliding windows/SW_l10.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int fn(vector<int>& nums, int goal){
+        if(goal<0) return 0;
+        int l=0, r=0, sum=0, cnt=0;
+        int n=nums.size();
+        while (r<n){
+            sum+=(nums[r]%2);
+            while(sum>goal){
+                sum=sum-(nums[l]%2);
+                l++;
+            }
+            cnt+=r-l+1;
+            r++;
+        }
+        return cnt;
+}
+int numberOfSubarrays(vector<int>& nums, int k) {
+        return fn(nums, k)-fn(nums,k-1);
+}
